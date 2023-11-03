25dad3185f1d91dccbdfab8f4f21b6908039dfab
Dylan Ngo <it.tinhngo@gmail.com>
2023-11-03 17:29:07 +0800

    init project

---
+++ src/main/java/com/dylanops/review/api/UserResource.java
@@ -0,0 +1,39 @@
+package com.dylanops.review.api;
+
+import io.quarkus.security.identity.SecurityIdentity;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.NoCache;
+
+@Path("/api/users")
+public class UserResource {
+
+    @Inject
+    SecurityIdentity securityIdentity;
+
+    @GET
+    @Path("/me")
+    @RolesAllowed("user")
+    @NoCache
+    @Produces(MediaType.APPLICATION_JSON)
+    public User me() {
+        return new User(securityIdentity);
+    }
+
+    public static class User {
+
+        private final String userName;
+
+        User(SecurityIdentity securityIdentity) {
+            this.userName = securityIdentity.getPrincipal().getName();
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+    }
+}
