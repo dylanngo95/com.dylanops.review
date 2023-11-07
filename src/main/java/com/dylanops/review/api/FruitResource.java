59bd1f5657861f1b93c7a159c20474be1713bed4
Dylan Ngo <it.tinhngo@gmail.com>
2023-11-07 14:24:47 +0800

    update static UI

---
+++ src/main/java/com/dylanops/review/api/FruitResource.java
@@ -0,0 +1,72 @@
+package com.dylanops.review.api;
+
+import io.quarkus.runtime.StartupEvent;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import jakarta.ws.rs.core.Response.ResponseBuilder;
+
+import java.net.URI;
+
+@Path("/fruits")
+public class FruitResource {
+
+    @Inject
+    io.vertx.mutiny.mysqlclient.MySQLPool client;
+
+    @Inject
+    @ConfigProperty(name = "review.schema.create", defaultValue = "true")
+    boolean schemaCreate;
+
+    void config(@Observes StartupEvent ev) {
+        if (schemaCreate) {
+            initdb();
+        }
+    }
+
+    private void initdb() {
+        client.query("DROP TABLE IF EXISTS fruits").execute()
+                .flatMap(r -> client.query("CREATE TABLE fruits (id SERIAL PRIMARY KEY, name TEXT NOT NULL)").execute())
+                .flatMap(r -> client.query("INSERT INTO fruits (name) VALUES ('Orange')").execute())
+                .flatMap(r -> client.query("INSERT INTO fruits (name) VALUES ('Pear')").execute())
+                .flatMap(r -> client.query("INSERT INTO fruits (name) VALUES ('Apple')").execute())
+                .await().indefinitely();
+    }
+
+    @GET
+    public Multi<Fruit> get() {
+        return Fruit.findAll(client);
+    }
+
+    @GET
+    @Path("{id}")
+    public Uni<Response> getSingle(Long id) {
+        return Fruit.findById(client, id)
+                .onItem().transform(fruit -> fruit != null ? Response.ok(fruit) : Response.status(Response.Status.NOT_FOUND))
+                .onItem().transform(ResponseBuilder::build);
+    }
+
+    @POST
+    public Uni<Response> create(Fruit fruit) {
+        return fruit.save(client)
+                .onItem().transform(id -> URI.create("/fruits/" + id))
+                .onItem().transform(uri -> Response.created(uri).build());
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Uni<Response> delete(Long id) {
+        return Fruit.delete(client, id)
+                .onItem().transform(deleted -> deleted ? Response.Status.NO_CONTENT : Response.Status.NOT_FOUND)
+                .onItem().transform(status -> Response.status(status).build());
+    }
+
+}
---
+++ src/main/java/com/dylanops/review/api/StaticResources.java
@@ -0,0 +1,17 @@
+package com.dylanops.review.api;
+
+import io.quarkus.runtime.StartupEvent;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+import jakarta.enterprise.event.Observes;
+
+public class StaticResources {
+
+
+    void installRoute(@Observes StartupEvent startupEvent, Router router) {
+        router.route()
+                .path("/static/*")
+                .handler(StaticHandler.create("static/"));
+    }
+
+}
