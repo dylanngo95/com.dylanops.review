23d25a78188c0d87b759383bf42870b46e146226
Dylan Ngo <it.tinhngo@gmail.com>
2023-11-07 15:52:47 +0800

    remove oidc

--- src/main/java/com/dylanops/review/api/AdminResource.java
+++ src/main/java/com/dylanops/review/api/AdminResource.java
@@ -1,6 +1,5 @@
 package com.dylanops.review.api;
 
-import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -10,7 +9,6 @@ import jakarta.ws.rs.core.MediaType;
 public class AdminResource {
 
     @GET
-    @RolesAllowed("admin")
     @Produces(MediaType.TEXT_PLAIN)
     public String admin() {
         return "granted";
--- src/main/java/com/dylanops/review/api/UserResource.java
+++ src/main/java/com/dylanops/review/api/UserResource.java
@@ -1,8 +1,5 @@
 package com.dylanops.review.api;
 
-import io.quarkus.security.identity.SecurityIdentity;
-import jakarta.annotation.security.RolesAllowed;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -12,24 +9,20 @@ import org.jboss.resteasy.reactive.NoCache;
 @Path("/api/users")
 public class UserResource {
 
-    @Inject
-    SecurityIdentity securityIdentity;
-
     @GET
     @Path("/me")
-    @RolesAllowed("user")
     @NoCache
     @Produces(MediaType.APPLICATION_JSON)
     public User me() {
-        return new User(securityIdentity);
+        return new User();
     }
 
     public static class User {
 
         private final String userName;
 
-        User(SecurityIdentity securityIdentity) {
-            this.userName = securityIdentity.getPrincipal().getName();
+        User() {
+            this.userName = "Dylan";
         }
 
         public String getUserName() {
